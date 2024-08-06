b52d366cbf32a94805fe5e92fe0b4ab882309903
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-08-06 10:16:32 +0200

    Unzip compressed FASTA file

--- get_fasta.sh
+++ get_fasta.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Get the FASTA file suggested by the user
 wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_37/GRCh38.p13.genome.fa.gz
+gzip -d GRCh38.p13.genome.fa.gz
