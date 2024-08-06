bfe622781672fb361e38a9754a9bcfaebbfba472
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-08-06 12:07:03 +0200

    Add 'create_jvarkit_sif.sh'

---
+++ create_jvarkit_sif.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Create a jvarkit Singularity container
+#
+# Documentation at https://docs.uppmax.uu.se/software/jvarkit
+#
+sudo singularity build jvarkit.sif docker:lindenb/jvarkit
